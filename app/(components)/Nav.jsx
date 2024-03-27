c3500e81aaf2e780fe58993ca1eea1202c14d2c7
Moballigh5225 <moballigh5225@gmail.com>
2024-03-28 02:05:02 +0530

    structure created

--- app/(components)/Nav.jsx
+++ app/(components)/Nav.jsx
@@ -14,7 +14,7 @@ const Nav = () => {
         </Link>
       </div>
       <div>
-        <p className="text-default-text"></p>
+        <p className="text-default-text">mobi@gmail.com</p>
       </div>
     </nav>
   );
